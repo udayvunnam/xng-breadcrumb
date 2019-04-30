dc75999e388674395b55b11d82cd5c99ef646832
Uday Vunnam <uday.kumar@bhge.com>
2019-04-30 15:00:01 +0530

    breadcrumb setup

--- projects/xng-breadcrumb/src/lib/breadcrumb.component.ts
+++ projects/xng-breadcrumb/src/lib/breadcrumb.component.ts
@@ -12,17 +12,21 @@ export class BreadcrumbComponent implements OnInit {
   breadcrumbs$ = this.router.events.pipe(
     filter(event => event instanceof NavigationEnd),
     distinctUntilChanged(),
-    map(event => this.buildBreadCrumb(this.activatedRoute.root))
+    map(event => this.setBreadcrumb(this.activatedRoute.root))
   );
 
   constructor(private activatedRoute: ActivatedRoute, private router: Router) {}
 
   ngOnInit() {}
 
-  buildBreadCrumb(route: ActivatedRoute, url: string = '', breadcrumbs: Array<Breadcrumb> = []): Array<Breadcrumb> {
-    // If no routeConfig is avalailable we are on the root path
-    const label = route.routeConfig ? route.routeConfig.data['breadcrumb'] : 'Home';
+  setBreadcrumb(route: ActivatedRoute, url: string = '', breadcrumbs: Breadcrumb[] = []): Breadcrumb[] {
     const path = route.routeConfig ? route.routeConfig.path : '';
+    //set label as path if a breadcrumb value is not provided.
+    let label = path || 'Home';
+    if (route.routeConfig && route.routeConfig.data && route.routeConfig.data.breadcrumb) {
+      label = route.routeConfig.data['breadcrumb'];
+    }
+
     // In the routeConfig the complete path is not available,
     // so we rebuild it each time
     const nextUrl = `${url}${path}/`;
@@ -34,7 +38,7 @@ export class BreadcrumbComponent implements OnInit {
     if (route.firstChild) {
       // If we are not on our current path yet,
       // there will be more children to look after, to build our breadcumb
-      return this.buildBreadCrumb(route.firstChild, nextUrl, newBreadcrumbs);
+      return this.setBreadcrumb(route.firstChild, nextUrl, newBreadcrumbs);
     }
     return newBreadcrumbs;
   }
