fc741f86bee88d65154bbf74efc4cb6d2ffc1011
udayvunnam <uday4vunnam@gmail.com>
2019-05-01 21:05:47 +0530

    fix(ci):fixing tests running forever

--- projects/xng-breadcrumb-app/karma.conf.js
+++ projects/xng-breadcrumb-app/karma.conf.js
@@ -1,7 +1,7 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-module.exports = function (config) {
+module.exports = function(config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
@@ -24,9 +24,9 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
+    autoWatch: false,
     browsers: ['Chrome'],
-    singleRun: false,
-    restartOnFileChange: true
+    singleRun: true,
+    restartOnFileChange: false
   });
 };
