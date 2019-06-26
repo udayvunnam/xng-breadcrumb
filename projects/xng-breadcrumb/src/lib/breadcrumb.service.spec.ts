d748db2cdfde01c5f1735c3097248d812d11e89c
Uday Vunnam <uday.kumar@bhge.com>
2019-06-26 20:14:24 +0530

    tests: fixing tests for library

--- projects/xng-breadcrumb/src/lib/breadcrumb.service.spec.ts
+++ projects/xng-breadcrumb/src/lib/breadcrumb.service.spec.ts
@@ -1,9 +1,15 @@
 import { TestBed } from '@angular/core/testing';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { BreadcrumbService } from './breadcrumb.service';
 
 describe('XngBreadcrumbService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [CommonModule, RouterModule.forRoot([])]
+    })
+  );
 
   it('should be created', () => {
     const service: BreadcrumbService = TestBed.get(BreadcrumbService);
