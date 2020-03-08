420b1a04adbe5eb8172205b4c53cdaa7467afe68
Uday Vunnam <uday4vunnam@gmail.com>
2020-03-08 18:52:21 +0530

    ci: danger js setup

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,4 +1,4 @@
 import { message, danger } from 'danger';
 
-const modifiedMD = danger.git.modified_files.join('- ');
-message('Changed files in this PR: \n - ' + modifiedMD);
+const modifiedMD = danger.git.modified_files.join('\n- ');
+message('Changed files in this PR: ' + modifiedMD);
