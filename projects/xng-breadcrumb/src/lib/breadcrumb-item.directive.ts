730a3091a4f6758d165824e4fb895fdb44dda785
Uday Vunnam <uday4vunnam@gmail.com>
2020-03-15 22:48:26 +0530

    preserve query params and provide disable link functionality

--- projects/xng-breadcrumb/src/lib/breadcrumb-item.directive.ts
+++ projects/xng-breadcrumb/src/lib/breadcrumb-item.directive.ts
@@ -1,5 +1,10 @@
 import { Directive } from '@angular/core';
 
+/**
+ * This directive is used to customize the breadcrumb label behavior
+ * *xngBreadcrumbItem directive can be used in the child element of xng-breadcrumb
+ * Usage: refer to demo app.component.html
+ */
 @Directive({
   selector: '[xngBreadcrumbItem]'
 })
--- projects/xng-breadcrumb/src/lib/breadcrumb.module.ts
+++ projects/xng-breadcrumb/src/lib/breadcrumb.module.ts
@@ -3,10 +3,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { BreadcrumbItemDirective } from './breadcrumb-item.directive';
 import { BreadcrumbComponent } from './breadcrumb.component';
-import { AutoLabelPipe } from './auto-label.pipe';
 
 @NgModule({
-  declarations: [BreadcrumbComponent, BreadcrumbItemDirective, AutoLabelPipe],
+  declarations: [BreadcrumbComponent, BreadcrumbItemDirective],
   imports: [CommonModule, RouterModule],
   exports: [BreadcrumbComponent, BreadcrumbItemDirective]
 })
