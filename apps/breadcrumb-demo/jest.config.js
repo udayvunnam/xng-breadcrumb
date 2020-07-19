8fa2defdde173ae1778f27a66a49f39fc74132f1
Uday Vunnam <uday4vunnam@gmail.com>
2020-07-19 07:11:09 +0530

    initial commit

---
+++ apps/breadcrumb-demo/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: 'breadcrumb-demo',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/apps/breadcrumb-demo',
+  snapshotSerializers: [
+    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+    'jest-preset-angular/build/AngularSnapshotSerializer.js',
+    'jest-preset-angular/build/HTMLCommentSerializer.js',
+  ],
+};
