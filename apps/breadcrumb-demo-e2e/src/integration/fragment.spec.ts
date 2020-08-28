7ba9e7ba3a3448411ad794edb5b0bc4bd7ffd312
Uday Vunnam <20707504+udayvunnam@users.noreply.github.com>
2020-08-28 10:19:41 +0530

    feat: provide ability to disable fragment (#42)

---
+++ apps/breadcrumb-demo-e2e/src/integration/fragment.spec.ts
@@ -0,0 +1,29 @@
+import { getDefaultBreadcrumbs } from '../support/app.po';
+
+describe('breadcrumb-demo', () => {
+  it('should preserve fragments by default', () => {
+    cy.visit('/');
+    cy.get('.navbar-header').contains('Mentors').click();
+    cy.location().should((loc) => {
+      expect(loc.hash).to.eq('#testFragment');
+    });
+    cy.get('bd-mentor-list .mat-card-avatar').eq(0).click();
+    getDefaultBreadcrumbs().contains('Enabler').click();
+    cy.location().should((loc) => {
+      expect(loc.hash).to.eq('#testFragment');
+    });
+  });
+
+  it('should not preserve fragments for disabled Fragments', () => {
+    cy.visit('/');
+    cy.get('.navbar-header').contains('Mentors').click();
+    cy.location().should((loc) => {
+      expect(loc.hash).to.eq('#testFragment');
+    });
+    cy.get('bd-mentor-list .mat-card-avatar').eq(0).click();
+    cy.get('#advancedTemplate2').contains('Enabler').click();
+    cy.location().should((loc) => {
+      expect(loc.hash).to.eq('');
+    });
+  });
+});
