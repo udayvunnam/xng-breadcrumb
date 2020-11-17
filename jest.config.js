e7d728a92de73e529ceab806e38d4e8e71dfc0a7
Uday Vunnam <uday4vunnam@gmail.com>
2020-11-17 07:10:08 +0530

    fix: remove empty breadcrumb displayed at start

--- jest.config.js
+++ jest.config.js
@@ -6,4 +6,5 @@ module.exports = {
   resolver: '@nrwl/jest/plugins/resolver',
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageReporters: ['html'],
+  projects: '<rootDir>/apps/simple-demo',
 };
