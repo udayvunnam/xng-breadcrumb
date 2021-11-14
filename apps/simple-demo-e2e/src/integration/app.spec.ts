573778d7911709e0e1426c22ecd34cdc862064a6
Uday Vunnam <uday4vunnam@gmail.com>
2021-11-14 21:17:28 +0530

    fix: tests

--- apps/simple-demo-e2e/src/integration/app.spec.ts
+++ apps/simple-demo-e2e/src/integration/app.spec.ts
@@ -1,7 +1,11 @@
 describe('simple-demo', () => {
   it('should contain breadcrumbs for books', () => {
     cy.visit('/');
+    // initially within ngIf and not shown
+    cy.get('xng-breadcrumb').should('not.exist');
+    cy.get('button').contains('Toggle Breadcrumb Visibility').click();
     cy.get('xng-breadcrumb').contains('Dashboard');
+
     // shouldn't have default seperator if Home breadcrumb is not defined
     cy.get('.xng-breadcrumb-list').contains('/').should('not.exist');
     cy.get('a').contains('Order Details').click();
