d459860dfa8dda244472571143c23aef31b26f12
Aliaksandr Zasim <al.zasim@gmail.com>
2023-10-05 16:19:09 +0200

    fix(lib): correctly process routes with empty path (#177)
    Co-authored-by: Aliaksandr Zasim <aliaksandr.zasim@autorabit.com>

--- libs/xng-breadcrumb/src/lib/breadcrumb.service.ts
+++ libs/xng-breadcrumb/src/lib/breadcrumb.service.ts
@@ -145,7 +145,7 @@ export class BreadcrumbService {
     activatedRouteSnapshot: ActivatedRouteSnapshot,
     routeLinkPrefix: string
   ): Breadcrumb[] | void {
-    if (activatedRouteSnapshot.routeConfig?.path) {
+    if (activatedRouteSnapshot.routeConfig?.path != null) {
       const breadcrumbItem = this.prepareBreadcrumbItem(
         activatedRouteSnapshot,
         routeLinkPrefix
