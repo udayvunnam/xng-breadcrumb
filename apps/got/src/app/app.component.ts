e7b806ba96c2a0a4b35f4cd9ea14cc9af092998b
Uday Vunnam <uday4vunnam@gmail.com>
2024-03-28 18:25:30 +0530

    feat: initialze app

---
+++ apps/got/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NxWelcomeComponent } from './nx-welcome.component';
+
+@Component({
+  standalone: true,
+  imports: [NxWelcomeComponent, RouterModule],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styles: ``,
+})
+export class AppComponent {
+  title = 'game-of-thrones';
+}
---
+++ apps/minimal/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  standalone: true,
+  imports: [RouterModule],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styles: ``,
+})
+export class AppComponent {}
