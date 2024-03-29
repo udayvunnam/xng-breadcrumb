7f9bcc28fd63380fe2f28db8a582138191ccfcc9
Uday Vunnam <uday4vunnam@gmail.com>
2024-03-29 14:39:28 +0530

    10.0.2

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 
-# Checkout main branch
 git checkout main
-
-# Pull latest changes from origin main
 git pull origin main
 
-# Copy README.md to libs/xng-breadcrumb/README.md
-cp README.md libs/xng-breadcrumb/README.md
+cp README.md xng-breadcrumb/README.md
 
 # Get npm package version
 VERSION=$(npm -s run env | grep npm_package_version | sed 's/npm_package_version=//')
+# Update npm version in libs/xng-breadcrumb directory
+(
+  cd libs/xng-breadcrumb
+  npm version "$VERSION"
+)
 
-# Change directory to libs/xng-breadcrumb and update npm version
-cd libs/xng-breadcrumb
-npm version "$VERSION"
-cd ..
-
-# Add changes, commit, and push to origin main
 git add .
 git commit -m "$VERSION"
 git push origin main
