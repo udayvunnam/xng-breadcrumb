4447446b5c2263e657ae31cc74bc35ea61fedc18
Uday Vunnam <uday4vunnam@gmail.com>
2026-03-08 07:51:53 +0530

    fixes https://github.com/udayvunnam/xng-breadcrumb/issues/229 https://github.com/udayvunnam/xng-breadcrumb/issues/223

--- xng-breadcrumb/src/lib/breadcrumb.component.spec.ts
+++ xng-breadcrumb/src/lib/breadcrumb.component.spec.ts
@@ -1,13 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute, Router } from '@angular/router';
+import { EMPTY } from 'rxjs';
 import { BreadcrumbComponent } from './breadcrumb.component';
 
 describe('BreadcrumbComponent', () => {
   let component: BreadcrumbComponent;
   let fixture: ComponentFixture<BreadcrumbComponent>;
+  const activatedRouteMock = {
+    snapshot: {
+      routeConfig: undefined,
+      firstChild: undefined,
+      params: {},
+      queryParams: {},
+      fragment: null,
+    },
+    params: EMPTY,
+  };
+  const routerMock = {
+    config: [],
+    events: EMPTY,
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [BreadcrumbComponent],
+      providers: [
+        { provide: ActivatedRoute, useValue: activatedRouteMock },
+        { provide: Router, useValue: routerMock },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(BreadcrumbComponent);
--- xng-breadcrumb/src/lib/breadcrumb.service.spec.ts
+++ xng-breadcrumb/src/lib/breadcrumb.service.spec.ts
@@ -1,12 +1,33 @@
 import { TestBed } from '@angular/core/testing';
+import { ActivatedRoute, Router } from '@angular/router';
+import { EMPTY } from 'rxjs';
 
 import { BreadcrumbService } from './breadcrumb.service';
 
 describe('BreadcrumbService', () => {
   let service: BreadcrumbService;
+  const activatedRouteMock = {
+    snapshot: {
+      routeConfig: undefined,
+      firstChild: undefined,
+      params: {},
+      queryParams: {},
+      fragment: null,
+    },
+    params: EMPTY,
+  };
+  const routerMock = {
+    config: [],
+    events: EMPTY,
+  };
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: ActivatedRoute, useValue: activatedRouteMock },
+        { provide: Router, useValue: routerMock },
+      ],
+    });
     service = TestBed.inject(BreadcrumbService);
   });
 
