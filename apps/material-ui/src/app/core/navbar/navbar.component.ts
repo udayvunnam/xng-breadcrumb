ccde8849daaf411c2c4fee6e6531ef527151ffe8
Uday Vunnam <uday4vunnam@gmail.com>
2026-03-09 11:34:38 +0530

    update

--- apps/material-ui/src/app/core/navbar/navbar.component.ts
+++ apps/material-ui/src/app/core/navbar/navbar.component.ts
@@ -1,7 +1,9 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Output } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { MatTooltip } from '@angular/material/tooltip';
 import { RouterLink, RouterModule } from '@angular/router';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconRegistry } from '@angular/material/icon';
 
 import { MatAnchor, MatIconAnchor, MatIconButton } from '@angular/material/button';
 import { MatToolbar } from '@angular/material/toolbar';
@@ -14,8 +16,15 @@ import { MatToolbar } from '@angular/material/toolbar';
 })
 export class NavbarComponent {
   theme = 'light';
+  private readonly iconRegistry = inject(MatIconRegistry);
+  private readonly sanitizer = inject(DomSanitizer);
 
   @Output() themeChanged: EventEmitter<string> = new EventEmitter();
+
+  constructor() {
+    this.iconRegistry.addSvgIcon('twitter', this.sanitizer.bypassSecurityTrustResourceUrl('assets/img/twitter.svg'));
+    this.iconRegistry.addSvgIcon('github', this.sanitizer.bypassSecurityTrustResourceUrl('assets/img/github.svg'));
+  }
 
   toggleTheme() {
     this.theme = this.theme === 'light' ? 'dark' : 'light';
